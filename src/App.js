4c0485ea691f590cdc5025d8881626d083771594
Sujal-Jadhav <112196729+Sujal-Jadhav@users.noreply.github.com>
2022-10-05 07:25:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import register from './register';
+import ogin from './login';
+
+function App() {
+
+  return (
+    <main className="App">
+      <login />
+      </main>
+    
+  );
+}
+
+export default App;
